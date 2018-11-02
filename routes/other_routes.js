8288138432ed5ae80958ab8e7920e1e4f872e00d
Fantasy Inferno <fantasyinferno@gmail.com>
2018-11-02 20:04:45 +0700

    Updated homepage

---
+++ routes/other_routes.js
@@ -0,0 +1,15 @@
+/** Express router providing user-related routes
+ * @module routers/users
+ * @requires express
+ * @requires axios
+ * @requires async
+ * @requires lodash
+ */
+const axios = require('axios');
+const _ = require('lodash');
+const async = require('async');
+module.exports = (app, admin) => {
+    app.get('/', (req, res) => {
+        res.render('homepage');
+    });
+}
